5e930507729ca66fdedb593b813729e66fcec7d5
Lizaveta_Zhukouskaya <lizaveta.zhukouskaya@gmail.com>
2022-02-10 14:10:29 +0300

    ошибка 1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import {HashRouter, Routes, Route } from 'react-router-dom';
 
-import MainPage from './components/pages/MainPage/MainPage.jsx';
-import Header from './components/header/header/Header.jsx';
-import WomenPage from './components/pages/WomenPage.jsx';
-import MenPage from './components/pages/MenPage.jsx';
+import MainPage from './components/pages/MainPage/MainPage';
+import Header from './components/header/header/Header';
+import WomenPage from './components/pages/WomenPage';
+import MenPage from './components/pages/MenPage';
 
 // import NotfoundPage from './components/pages/NotfoundPage';
 
