0540d4b2f7c997ec9b86ae3fe99a2d7e9b8d696c
Lizaveta_Zhukouskaya <lizaveta.zhukouskaya@gmail.com>
2022-02-11 13:28:20 +0300

    finish mainPage, add blog section

--- src/components/pages/MainPage/blog/blogPosts/BlogPosts.jsx
+++ src/components/pages/MainPage/blog/blogPosts/BlogPosts.jsx
@@ -4,16 +4,16 @@ import './blogPosts.css'
 
 import Post from './post/Post';
 
-import Post1 from './img/1.jpg'
-import Post2 from './img/2.jpg'
-import Post3 from './img/3.jpg'
+import post1 from './img/post1.jpg'
+import post2 from './img/post2.jpg'
+import post3 from './img/post3.jpg'
 
 const BlogPosts = () => {
   return (
-    <div className='blogPosts'>
-      <Post image= {Post1} title='The Easiest Way to Break' text='But I must explain to you how all this mistaken idea of denouncing pleas and praising pain was bor' />
-      <Post image= {Post2} title='Wedding Season' text='But I must explain to you how all this mistaken idea of denouncing pleas and praising pain was bor' />
-      <Post image= {Post3} title='Recent Favorites On Repeat' text='But I must explain to you how all this mistaken idea of denouncing pleas and praising pain was bor' />
+    <div className='blog__posts'>
+      <Post img= {post1} title='The Easiest Way to Break' text='But I must explain to you how all this mistaken idea of denouncing pleas and praising pain was bor' />
+      <Post img= {post2} title='Wedding Season' text='But I must explain to you how all this mistaken idea of denouncing pleas and praising pain was bor' />
+      <Post img= {post3} title='Recent Favorites On Repeat' text='But I must explain to you how all this mistaken idea of denouncing pleas and praising pain was bor' />
     </div>
   );
 };
--- src/components/pages/MainPage/blog/blogPosts/post/Post.jsx
+++ src/components/pages/MainPage/blog/blogPosts/post/Post.jsx
@@ -5,7 +5,7 @@ import './post.css'
 const Post = (p) => {
   return (
     <div className='post'>
-      <imag className='post_img' src= {p.image} alt='image post' />
+      <img className='post_img' src={p.img} alt='image post' />
       <div className='post_info'>
         <div className='post_title'>{p.title}</div>
         <div className='post_text'>{p.text}</div>
