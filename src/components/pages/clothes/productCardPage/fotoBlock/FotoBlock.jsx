9e2abc8b694b6b6944b94f53724bda8791b0c705
Lizaveta_Zhukouskaya <lizaveta.zhukouskaya@gmail.com>
2022-02-22 15:42:16 +0300

    add swiper in foto front block

--- src/components/pages/clothes/productCardPage/fotoBlock/FotoBlock.jsx
+++ src/components/pages/clothes/productCardPage/fotoBlock/FotoBlock.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 
 import './fotoBlock.css'
 
-import foto1 from './../img/clothes/Rectangle 1.jpg'
-import foto2 from './../img/clothes/Rectangle 32.jpg'
-import foto3 from './../img/clothes/Rectangle 33.jpg'
-import foto4 from './../img/clothes/Rectangle 34.jpg'
-
 import up from './../img/up.svg'
 import down from './../img/down.svg'
 import FotoSwiper from './fotoSwiper/FotoSwiper';
@@ -24,10 +19,10 @@ const FotoBlock = (p) => {
           <img src= { down } alt='down'/>
           </button>
         </div>
-        <img className='foto1' src= { foto1 } alt='foto_product' />
-        <img className='foto2' src= { foto2 } alt='foto_product' />
-        <img className='foto3' src= { foto3 } alt='foto_product' />
-        <img className='foto4' src= { foto4 } alt='foto_product' />
+        <img className='front' src= { `https://training.cleverland.by/shop${ p.images[0].url }` } alt='foto_product' />
+        <img className='front' src= { `https://training.cleverland.by/shop${ p.images[0].url }` } alt='foto_product' />
+        <img className='front' src= { `https://training.cleverland.by/shop${ p.images[0].url }` } alt='foto_product' />
+        <img className='front' src= { `https://training.cleverland.by/shop${ p.images[0].url }` } alt='foto_product' />
       </div>
 
     </div>
